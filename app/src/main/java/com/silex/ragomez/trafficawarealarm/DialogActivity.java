de5db935d09e9c9610a9856f75d80f688ba85ac5
Ray Gomez <rayvincent.gomez@gmail.com>
2015-11-12 21:47:17 +0800

    added vibrate.

--- app/src/main/java/com/silex/ragomez/trafficawarealarm/DialogActivity.java
+++ app/src/main/java/com/silex/ragomez/trafficawarealarm/DialogActivity.java
@@ -1,11 +1,13 @@
 package com.silex.ragomez.trafficawarealarm;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Vibrator;
 
 import com.example.android.common.activities.SampleActivityBase;
 
@@ -20,12 +22,17 @@ public class DialogActivity extends SampleActivityBase {
         final Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
         r.play();
 
+        final Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        final long[] pattern = {0, 100, 1000};
+        v.vibrate(pattern, 0);
+
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage("Time's Up").setCancelable(
                 false).setPositiveButton("Cancel Alarm",
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         r.stop();
+                        v.cancel();
                         finish();
                     }
                 });
