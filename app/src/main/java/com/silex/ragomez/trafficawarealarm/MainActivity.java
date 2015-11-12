bf53c97554b0b8589b738ffc540ee7b3c959c018
Ray Gomez <rayvincent.gomez@gmail.com>
2015-11-12 10:24:16 +0800

    compute the destination.

--- app/src/main/java/com/silex/ragomez/trafficawarealarm/MainActivity.java
+++ app/src/main/java/com/silex/ragomez/trafficawarealarm/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.text.Editable;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
@@ -60,8 +59,8 @@ public class MainActivity extends SampleActivityBase implements GoogleApiClient.
         destinationView = (AutoCompleteTextView) findViewById(R.id.destination);
 
         // Register a listener that receives callbacks when a suggestion has been selected
-        originView.setOnItemClickListener(mAutocompleteClickListener);
-        destinationView.setOnItemClickListener(mAutocompleteClickListener);
+        originView.setOnItemClickListener(originViewClickListener);
+        destinationView.setOnItemClickListener(destinationViewClickListener);
 
         // Set up the adapter that will retrieve suggestions from the Places Geo Data API that cover
         // the entire world.
@@ -94,17 +93,13 @@ public class MainActivity extends SampleActivityBase implements GoogleApiClient.
         createAlarm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // check details
-                originView.getText();
-                destinationView.getText();
-                EditText time = (EditText) findViewById(R.id.time);
-                EditText date = (EditText) findViewById(R.id.target_date);
 
+                // check details
                 // send post with input details
                 String path = String.format("http://silex-archnat.rhcloud.com/rest/api/v1/compute_travel_time?to=%f,%f&from=%f,%f",
                         destination.latitude, destination.longitude, origin.latitude, origin.longitude);
                 Log.i(TAG, "path:" + path);
-                new HttpAsyncTask().execute("http://silex-archnat.rhcloud.com/rest/api/v1/compute_travel_time?to=14.661258%2C121.061674&from=14.558194%2C121.085495");
+                new HttpAsyncTask().execute(path);
 
             }
         });
@@ -135,7 +130,6 @@ public class MainActivity extends SampleActivityBase implements GoogleApiClient.
     EditText target_date;
     EditText target_time;
 
-
     /**
      * Listener that handles selections from suggestions from the AutoCompleteTextView that
      * displays Place suggestions.
@@ -145,7 +139,7 @@ public class MainActivity extends SampleActivityBase implements GoogleApiClient.
      * @see com.google.android.gms.location.places.GeoDataApi#getPlaceById(com.google.android.gms.common.api.GoogleApiClient,
      * String...)
      */
-    private AdapterView.OnItemClickListener mAutocompleteClickListener
+    private AdapterView.OnItemClickListener originViewClickListener
             = new AdapterView.OnItemClickListener() {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -166,7 +160,7 @@ public class MainActivity extends SampleActivityBase implements GoogleApiClient.
               */
             PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi
                     .getPlaceById(mGoogleApiClient, placeId);
-            placeResult.setResultCallback(mUpdatePlaceDetailsCallback);
+            placeResult.setResultCallback(originUpdatePlaceDetailsCallback);
 
             Toast.makeText(getApplicationContext(), "Clicked: " + primaryText,
                     Toast.LENGTH_SHORT).show();
@@ -174,11 +168,39 @@ public class MainActivity extends SampleActivityBase implements GoogleApiClient.
         }
     };
 
+    private AdapterView.OnItemClickListener destinationViewClickListener
+            = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            /*
+             Retrieve the place ID of the selected item from the Adapter.
+             The adapter stores each Place suggestion in a AutocompletePrediction from which we
+             read the place ID and title.
+              */
+            final AutocompletePrediction item = mAdapter.getItem(position);
+            final String placeId = item.getPlaceId();
+            final CharSequence primaryText = item.getPrimaryText(null);
+
+            Log.i(TAG, "Autocomplete item selected: " + primaryText);
+
+            /*
+             Issue a request to the Places Geo Data API to retrieve a Place object with additional
+             details about the place.
+              */
+            PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi
+                    .getPlaceById(mGoogleApiClient, placeId);
+            placeResult.setResultCallback(destinationUpdatePlaceDetailsCallback);
+
+            Toast.makeText(getApplicationContext(), "Clicked: " + primaryText,
+                    Toast.LENGTH_SHORT).show();
+            Log.i(TAG, "Called getPlaceById to get Place details for " + placeId);
+        }
+    };
     /**
      * Callback for results from a Places Geo Data API query that shows the first place result in
      * the details view on screen.
      */
-    private ResultCallback<PlaceBuffer> mUpdatePlaceDetailsCallback
+    private ResultCallback<PlaceBuffer> originUpdatePlaceDetailsCallback
             = new ResultCallback<PlaceBuffer>() {
         @Override
         public void onResult(PlaceBuffer places) {
@@ -192,7 +214,6 @@ public class MainActivity extends SampleActivityBase implements GoogleApiClient.
             final Place place = places.get(0);
 
             origin = place.getLatLng();
-            destination = place.getLatLng();
 
             Log.i(TAG, "Place details received: " + place.getName());
             Log.i(TAG, "Location received: " + place.getLatLng().toString());
@@ -201,6 +222,27 @@ public class MainActivity extends SampleActivityBase implements GoogleApiClient.
         }
     };
 
+    private ResultCallback<PlaceBuffer> destinationUpdatePlaceDetailsCallback
+            = new ResultCallback<PlaceBuffer>() {
+        @Override
+        public void onResult(PlaceBuffer places) {
+            if (!places.getStatus().isSuccess()) {
+                // Request did not complete successfully
+                Log.e(TAG, "Place query did not complete. Error: " + places.getStatus().toString());
+                places.release();
+                return;
+            }
+            // Get the Place object from the buffer.
+            final Place place = places.get(0);
+
+            destination = place.getLatLng();
+
+            Log.i(TAG, "Place details received: " + place.getName());
+            Log.i( TAG, "Location received: " + place.getLatLng().toString());
+
+            places.release();
+        }
+    };
     /**
      * Called when the Activity could not connect to Google Play services and the auto manager
      * could resolve the error automatically.
@@ -262,6 +304,8 @@ public class MainActivity extends SampleActivityBase implements GoogleApiClient.
     }
 
     private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+
+        String message;
         @Override
         protected String doInBackground(String... urls) {
 
@@ -271,7 +315,7 @@ public class MainActivity extends SampleActivityBase implements GoogleApiClient.
         // onPostExecute displays the results of the AsyncTask.
         @Override
         protected void onPostExecute(String result) {
-            Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
+            Toast.makeText(getBaseContext(), "Estimated time: " + message + "seconds", Toast.LENGTH_LONG).show();
         }
 
         private String GET(String urlStr) {
@@ -295,6 +339,8 @@ public class MainActivity extends SampleActivityBase implements GoogleApiClient.
                     System.out.println("nice! urlConnection.responseCode: " + responseCode);
                     result = convertInputStreamToString(inputStream);
                     JSONObject json = new JSONObject(result);
+                    message = json.get("duration_in_seconds").toString();
+
                 } else {
                     android.util.Log.v("CatalogClient", "Response code:" + responseCode);
                     result = "oh noes";
