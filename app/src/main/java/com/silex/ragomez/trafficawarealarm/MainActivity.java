780fa9b55f389af501e51eaa62c1c5314cde870c
Ray Gomez <rayvincent.gomez@gmail.com>
2015-11-13 12:59:02 +0800

    fix imports.

--- app/src/main/java/com/silex/ragomez/trafficawarealarm/MainActivity.java
+++ app/src/main/java/com/silex/ragomez/trafficawarealarm/MainActivity.java
@@ -2,8 +2,6 @@ package com.silex.ragomez.trafficawarealarm;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
@@ -12,7 +10,6 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.NumberPicker;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.android.common.activities.SampleActivityBase;
