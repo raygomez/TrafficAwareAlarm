0bd57edc5fc390573540e173e33f2401f0faca73
Ray Gomez <rayvincent.gomez@gmail.com>
2015-11-23 18:26:18 +0800

    added list activity.

---
+++ app/src/main/java/com/silex/ragomez/trafficawarealarm/ListActivity.java
@@ -0,0 +1,20 @@
+package com.silex.ragomez.trafficawarealarm;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+
+    public void loadMainActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
